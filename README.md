cf34ed9c6494976aadccd537c920977a3f49e96b
Jungwoon Ko <j3ko@ucsd.edu>
2022-11-20 20:11:36 -0800

    part1 and part2

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Lab 8 - Starter
+
+1. Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+Answer: Within a Github action that runs whenever code is pushed.
+- If you run automated tests after all developemnt is completed, you might cannt find where problems occurs and hard to debugg. And if you run your test locally, it mgiht take a lot of times, and this could casue some problmes on your workflow.
+
+2. Would you use an end to end test to check if a function is returning the correct output?
+
+Answer: No
+
+3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+Answer: No
+- Units Test do test only individual parts of codes in encapsulated unit as it's mentioned in write up paper. So, "messeage" feature is inappropriate for Unit Test because this feature is process of various functions.
+
+4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Answer : Yes
+- I do not think complicate codes and functions are not neccesary to process the "max message length" feature. We just neet to get a input message, check if it is exceed to 80 characters and return boolean value. So, Unit Test is fit in this feature.
+
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,87 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+//Phone number
+test('(123)456-4890', () => {
+    expect(functions.isPhoneNumber('(123)456-4890')).toBe(true);
+});
+
+test('412-156-8320', () => {
+    expect(functions.isPhoneNumber('412-156-8320')).toBe(true);
+});
+
+test('0', () => {
+    expect(functions.isPhoneNumber('0')).toBe(true);
+});
+
+test('00', () => {
+    expect(functions.isPhoneNumber('00')).toBe(true);
+});
+
+//Email
+test('ggg@gmail.com', () => {
+    expect(functions.isEmail('ggg@gmail.com')).toBe(true);
+})
+
+test('hhh@hanmail.net', () => {
+    expect(functions.isEmail('hhh@hanmail.net')).toBe(true);
+})
+
+test('doordash', () => {
+    expect(functions.isEmail('doordash')).toBe(true);
+})
+
+test('@seagul', () => {
+    expect(functions.isEmail('@seagul')).toBe(true);
+})
+
+//Strong password
+test('A12334', () => {
+    expect(functions.isStrongPassword('A12334')).toBe(true);
+})
+
+test('bk39kiej', () => {
+    expect(functions.isStrongPassword('bk39kiej')).toBe(true);
+})
+
+test('1dddd', () => {
+    expect(functions.isStrongPassword('1dddd')).toBe(true);
+})
+
+test('dkghqpwoeihdkgapodshfkjwoiehdkghoaih', () => {
+    expect(functions.isStrongPassword('dkghqpwoeihdkgapodshfkjwoiehdkghoaih')).toBe(true);
+})
+
+//Date
+test('11/24/1995', () => {
+    expect(functions.isDate('11/24/1995')).toBe(true);
+})
+
+test('05/29/2008', () => {
+    expect(functions.isDate('05/29/2008')).toBe(true);
+})
+
+test('/22/2222', () => {
+    expect(functions.isDate('/22/2222')).toBe(true);
+})
+
+test('07/08/1', () => {
+    expect(functions.isDate('/07/08/1')).toBe(true);
+})
+
+//Hex color
+test('#Fa8072', () => {
+    expect(functions.isHexColor('#Fa8072')).toBe(true);
+})
+
+test('#BDB76B', () => {
+    expect(functions.isHexColor('#BDB76B')).toBe(true);
+})
+
+test('#1111', () => {
+    expect(functions.isHexColor('#1111')).toBe(true);
+})
+
+test('Hello', () => {
+    expect(functions.isHexColor('Hello')).toBe(true);
+})
